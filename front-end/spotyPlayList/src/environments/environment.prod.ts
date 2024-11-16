d45f61717a17f27edd8585150f5a972444eada28
Gaston <gaston@tdil.com.ar>
2024-11-16 16:24:05 -0300

    environment

---
+++ front-end/spotyPlayList/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  apiUrl: 'http://192.168.0.8:2222',
+};
---
+++ front-end/spotyPlayList/src/environments/environment.ts
@@ -0,0 +1,5 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  apiUrl: 'http://192.168.0.8:5000', // Reemplaza con la IP y el puerto de tu servidor back-end
+};
