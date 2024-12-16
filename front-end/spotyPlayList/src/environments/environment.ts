d196ca441cd4895849487f04f076db08a4e3a19b
Gaston <gaston@tdil.com.ar>
2024-12-16 15:10:09 -0300

    environment

--- front-end/spotyPlayList/src/environments/environment.ts
+++ front-end/spotyPlayList/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://festivalmusic.gaxoblanco.com:5000', // Reemplaza con la IP y el puerto de tu servidor back-end
+  apiUrl: 'http://festivalmusic.gaxoblanco.com', // Reemplaza con la IP y el puerto de tu servidor back-end
 };
